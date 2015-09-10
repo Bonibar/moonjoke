000498af7f583c6b878faf06f02714fe7cef9bfb
Gaëtan Masson <im.gaetanmasson@gmail.com>
2015-09-10 23:00:30 +0200

    Update schema with the new media_type attribute

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150909160240) do
+ActiveRecord::Schema.define(version: 20150910150033) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150909160240) do
     t.integer  "member_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "media_type"
   end
 
   add_index "contents", ["member_id"], name: "index_contents_on_member_id"
