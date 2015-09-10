19c1fa26cd8f00d5b156111c31c43ea29bef3e9e
Gaëtan Masson <im.gaetanmasson@gmail.com>
2015-09-10 11:44:54 +0200

    Add members to the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Member.create(pseudo: 'Foo', email: 'foo@bar.com', gravatar: nil, age: 20, moderator: false)
+Member.create(pseudo: 'Bar', email: 'bar@bar.com', gravatar: nil, age: 24, moderator: true)
